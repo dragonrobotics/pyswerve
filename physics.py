26870a99d1353bb096495b06def0f93cf8cc69e6
Sebastian Mobo <stmobo@gmail.com>
2018-01-11 12:13:34 -0600

    Fix physics.py code style

--- physics.py
+++ physics.py
@@ -1,16 +1,21 @@
 import math
-from pyfrc.physics import drivetrains
 
 
 def get_talon_target(hal_data, talon_id):
     hal_target = hal_data['CAN'][talon_id]['value']
     return hal_target
 
+
 def copy_talon_analog_pos(hal_data, talon_id):
-    hal_data['CAN'][talon_id]['analog_in_position'] = hal_data['CAN'][talon_id]['value']
+    hal_data['CAN'][talon_id]['analog_in_position'] = hal_data['CAN'][talon_id]['value']  # noqa:E501
 
 
-def four_motor_swerve_drivetrain(lr_motor, rr_motor, lf_motor, rf_motor, lr_angle, rr_angle, lf_angle, rf_angle, x_wheelbase=2, y_wheelbase=2, speed=5):
+# pulled from master branch on pyfrc; remove when latest version actually works
+def four_motor_swerve_drivetrain(
+    lr_motor, rr_motor, lf_motor, rf_motor,
+    lr_angle, rr_angle, lf_angle, rf_angle,
+    x_wheelbase=2, y_wheelbase=2, speed=5
+):
     # Calculate speed of each wheel
     lr = lr_motor * speed
     rr = rr_motor * speed
@@ -28,24 +33,37 @@ def four_motor_swerve_drivetrain(lr_motor, rr_motor, lf_motor, rf_motor, lr_angl
 
     # Calculates the Vx and Vy components
     # Sin an Cos inverted because forward is 0 on swerve wheels
-    Vx = (math.sin(lr_rad) * lr) + (math.sin(rr_rad) * rr) + (math.sin(lf_rad) * lf) + (math.sin(rf_rad) * rf)
-    Vy = (math.cos(lr_rad) * lr) + (math.cos(rr_rad) * rr) + (math.cos(lf_rad) * lf) + (math.cos(rf_rad) * rf)
+    Vx = (
+        (math.sin(lr_rad) * lr)
+        + (math.sin(rr_rad) * rr)
+        + (math.sin(lf_rad) * lf)
+        + (math.sin(rf_rad) * rf))
+    Vy = (
+        (math.cos(lr_rad) * lr)
+        + (math.cos(rr_rad) * rr)
+        + (math.cos(lf_rad) * lf)
+        + (math.cos(rf_rad) * rf))
 
-
-    # Adjusts the angle corresponding to a diameter that is perpendicular to the radius (add or subtract 45deg)
+    # Adjusts the angle corresponding to a diameter
+    # that is perpendicular to the radius (add or subtract 45deg)
     lr_rad = (lr_rad + (math.pi / 4)) % (2 * math.pi)
     rr_rad = (rr_rad - (math.pi / 4)) % (2 * math.pi)
     lf_rad = (lf_rad - (math.pi / 4)) % (2 * math.pi)
     rf_rad = (rf_rad + (math.pi / 4)) % (2 * math.pi)
 
     # Finds the rotational velocity by finding the torque and adding them up
-    Vw = wheelbase_radius * ((math.cos(lr_rad) * lr) + (math.cos(rr_rad) * -rr) + (math.cos(lf_rad) * lf) + (math.cos(rf_rad) * -rf))
+    Vw = wheelbase_radius * (
+            (math.cos(lr_rad) * lr)
+            + (math.cos(rr_rad) * -rr)
+            + (math.cos(lf_rad) * lf)
+            + (math.cos(rf_rad) * -rf))
 
     Vx *= 0.25
     Vy *= 0.25
     Vw *= 0.25
 
     return Vx, Vy, Vw
+
 
 class PhysicsEngine(object):
     #  mirrors swerve_config in robot.py (name, steer_id, drive_id)
@@ -58,7 +76,6 @@ class PhysicsEngine(object):
 
     chassis_length = 32
     chassis_width = 28
-
 
     def __init__(self, physics_controller):
         self.physics_controller = physics_controller
